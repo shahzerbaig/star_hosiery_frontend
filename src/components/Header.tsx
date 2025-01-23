43b343f9c4fa133bffaef816c62a1235d42ec23d
Shahzer Baig <shahzer.baig.sb@gmail.com>
2025-01-23 13:49:14 +0530

    Updated: Header route is updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { useRouter } from "next/navigation";
-
+import Link from "next/link";
 
 const Header = () => {
 
   const router = useRouter();
     return (
       <header className="p-4 bg-blue-600 text-white flex justify-between items-center">
-        <h1 className="text-xl font-bold">Star Hosiery</h1>
+        <Link href='/dashboard'><h1 className="text-xl font-bold">Star Hosiery</h1></Link>
         <button onClick={()=> router.push('/')} className="bg-white text-blue-600 px-4 py-2 rounded">Logout</button>
       </header>
     );
